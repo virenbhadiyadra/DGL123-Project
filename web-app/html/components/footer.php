afdb38e109c81b93e75bfec676aa6b877ecca9af
virenbhadiyadra <vbhadiyadra@northislandcollege.ca>
2023-11-07 15:52:46 +0530

    Create footer structure

---
+++ web-app/html/components/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer-logo">Recipe System</div>
+        <div class="footer-links">
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Service</a>
+        </div>
+    </div>
+</footer>
