9b5b237ab82d1356c4157312be4329068913629d
virenbhadiyadra <vbhadiyadra@northislandcollege.ca>
2023-11-07 15:47:05 +0530

    Create header structure

---
+++ web-app/html/components/header.php
@@ -0,0 +1,12 @@
+<header class="header">
+    <div class="container">
+        <div class="logo">Recipe System</div>
+        <nav class="menu">
+            <a href="#">Home</a>
+            <a href="#">Recipes</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+            <a href="login.php">Login</a>
+        </nav>
+    </div>
+</header>
