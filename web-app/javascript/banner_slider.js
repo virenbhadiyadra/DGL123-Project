18fd422a177883b02c445ced3b2b07328829fbe6
virenbhadiyadra <vbhadiyadra@northislandcollege.ca>
2023-11-07 16:24:47 +0530

    Add javascript for the banner slider

---
+++ web-app/javascript/banner_slider.js
@@ -0,0 +1,34 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    }
+    showSlide(currentSlide);
+}
+
+// Automatically advance slides every 3 seconds
+setInterval(nextSlide, 3000);
+
+// Show the initial slide
+showSlide(currentSlide);
