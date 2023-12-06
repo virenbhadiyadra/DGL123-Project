c30f3fc49cc9e55cca85eed80876c1403e0559c3
virenbhadiyadra <vbhadiyadra@northislandcollege.ca>
2023-12-06 11:28:29 +0530

    link header element to sub pages

--- web-app/html/components/header.php
+++ web-app/html/components/header.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="logo">Recipe System</div>
         <nav class="menu">
-            <a href="#">Home</a>
-            <a href="#">Recipes</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
+            <a href="home.php">Home</a>
+            <a href="receipes.php">Recipes</a>
+            <a href="aboutus.php">About</a>
+            <a href="contact.php">Contact</a>
             <a href="login.php">Login</a>
         </nav>
     </div>
